8824459158c7532c9ea98666f01501a3d514b02a
Roland Huang <roland@warp.dev>
2026-05-06 16:52:49 -0700

    add log

--- app/src/autoupdate/mod.rs
+++ app/src/autoupdate/mod.rs
@@ -143,6 +143,7 @@ impl AutoupdateState {
             return;
         }
         if FeatureFlag::Autoupdate.is_enabled() && AppExecutionMode::as_ref(ctx).can_autoupdate() {
+            log::info!("Starting autoupdate polling loop");
             self.polling_started = true;
             // Initiate the polling loop.
             self.poll_for_update(ctx);
