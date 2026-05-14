67fee5395df2f62737c662e535645290d875f701
ZacharyZcR <zacharyzcr1984@gmail.com>
2026-05-14 15:11:03 +0800

    Localize code review action buttons

--- app/src/code/view.rs
+++ app/src/code/view.rs
@@ -1065,7 +1065,7 @@ impl CodeView {
                                     ButtonVariant::Outlined,
                                     tab.mouse_state_handles.reject_mouse_state.clone(),
                                 )
-                                .with_text_label("Reject".to_string())
+                                .with_text_label(t!("code.reject").to_string())
                                 .build()
                                 .on_click(|ctx, _, _| {
                                     ctx.dispatch_typed_action(CodeViewAction::RejectPendingDiffs)
@@ -1083,7 +1083,7 @@ impl CodeView {
                                     ButtonVariant::Outlined,
                                     tab.mouse_state_handles.accept_mouse_state.clone(),
                                 )
-                                .with_text_label("Accept and save".to_string())
+                                .with_text_label(t!("code.accept_and_save").to_string())
                                 .build()
                                 .on_click(|ctx, _, _| {
                                     ctx.dispatch_typed_action(
