30675c915b2c4112eee0283acab5c2b24b58d708
ZacharyZcR <zacharyzcr1984@gmail.com>
2026-05-14 15:44:02 +0800

    Localize warpify banner action

--- app/src/terminal/view/block_banner/warpify.rs
+++ app/src/terminal/view/block_banner/warpify.rs
@@ -160,7 +160,7 @@ pub fn render_warpification_banner(
                 ButtonVariant::Text,
                 state.dont_ask_button_mouse_state.clone(),
             )
-            .with_text_label("Do not show again".to_owned())
+            .with_text_label(t!("warpify.do_not_show_again").to_string())
             .build()
             .on_click(move |ctx, _, _| {
                 ctx.dispatch_typed_action(TerminalAction::DismissWarpifyBanner(
